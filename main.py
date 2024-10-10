f43abfd49a20dfc5e8669657ce1745fad6a638f8
John E. Onias <119425650+JohnOnias@users.noreply.github.com>
2024-10-10 18:16:38 -0300

    ,

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Node:
 class Playlist:
     def __init__(self) -> None:
         self.head = None
+        self.tail = None
 
     def is_circular(self) -> bool:
         if self.head is None:
@@ -21,39 +22,60 @@ class Playlist:
             return self.head.prev is not None
 
     def insert_at_end(self, music: Music) -> None:
-        newNode = Node(self.data)
-        if not self.head:
-            self.head = newNode
-        else: 
-            current = self.head
-            while current.next != self.head:
-                    current = current.next
-            current.next = newNode
-            newNode.prev = current
-            newNode.next = self.head
-            self.head.prev = newNode
+        newNode = Node(music)
+        if self.head is None:
+            self.head = self.tail = newNode
+            return
+        
+        current = self.tail
+        while current.next is not None:
+            current = current.next
+
+        current.next = newNode
+        newNode.prev = current
+        # self.tail.prev = newNode
                     
 
-    def remove(self, music: Music) -> None:
-        newNode = Node(self.data)
-        current = newNode
-
-        while current.data.name != music.name:
-            previous = current
-            current = current.next
-            if current.data.name == music.name:
-                break
-        if current.data.name == music.nameÇ
-            previous.next = current.next
+    def remove(self, name) -> None:
+        current = self.head
+        temp = None
+        while current is not None:
+            if current.data.name != name:
+                temp = current
+                current = current.next
+            break
         
-        else: 
-            print("Musica não encontrada!\n")
-            
-
+        temp.next = current.next
+        current.next = temp
+        current.next.prev = temp
+        temp.prev = current
 
 
     def move_music(self, from_pos: int, dest_pos: int) -> None:
         pass
 
-    def list_all() -> None:
+    def list_all(self) -> None:
+        current = self.head
+
+        while current is not None:
+            print("----------MUSIC-----------")
+            print(current.data.name)
+            print(current.data.artist)
+            print(current.data.duration)
+
+            current = current.next
+            
         pass
+
+
+list_playlist = Playlist()
+
+list_playlist.insert_at_end(Music("Borbulhas de Amor", "Fagner", "4:15"))
+list_playlist.insert_at_end(Music("Boate Azul", "Leno Brega", "3:20"))
+list_playlist.insert_at_end(Music("Seu amor me pegou", "Pablo Vittar", "2:30"))
+list_playlist.list_all()
+
+print("###########TENTANDO REMOVER#######################")
+list_playlist.remove("Boate Azul")
+list_playlist.list_all()
+
